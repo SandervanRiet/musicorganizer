e098d9183a1a9234d15c62af1df20ff10dbeb9ef
SandervanRiet <r0903044@student.thomasore.be>
2021-11-15 16:04:23 +0100

    Main methode in Test vervolledigd

---
+++ src/PlayList.java
@@ -0,0 +1,9 @@
+public class PlayList {
+    private String name;
+    private MusicOrganizer organizer;
+    private Track tracks;
+
+    public PlayList(String name) {
+        this.name = name;
+    }
+}
